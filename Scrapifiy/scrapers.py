e12e6ec788f77471dfb77dfcbaa2c7545fb26ef0
Norbaeocystin <baranrastislav@gmail.com>
2018-12-27 19:05:24 +0100

    Update scrapers.py

--- Scrapifiy/scrapers.py
+++ Scrapifiy/scrapers.py
@@ -2,3 +2,6 @@ from bs4 import BeauitfulSoup
 import random
 import requests
 
+#header for request, for more options look here https://github.com/N0taN3rd/userAgentLists
+#
+HEADER = {'User-Agent':'Mozilla/5.0 (iPad; CPU OS 11_2_6 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) CriOS/65.0.3325.152 Mobile/15D100 Safari/604.1'}
