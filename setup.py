0a3caddf42b8f93099399a8d295b2e77f087cbdb
Norbaeocystin <baranrastislav@gmail.com>
2018-12-27 18:55:34 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scrapify",
+    install_requires=["pymongo","requests","bs4","selenium"],
+    version="0.0.1",
+    author="Rastislav_Baran",
+    author_email="baranrastislav@gmail.com",
+    description="Python classes to help with scraping data from internet ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
