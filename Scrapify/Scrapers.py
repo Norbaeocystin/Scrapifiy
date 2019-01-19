2232590f7b58ec3cda9c031e4afd2b94136f3a35
Norbaeocystin <baranrastislav@gmail.com>
2019-01-19 11:15:46 +0100

    Update Scrapers.py

--- Scrapify/Scrapers.py
+++ Scrapify/Scrapers.py
@@ -1,3 +1,8 @@
+'''
+date: January 2019
+purpose: classes to help with scraping tasks
+version: 1.0.0
+'''
 from bs4 import BeautifulSoup
 import random
 import re
@@ -222,6 +227,86 @@ class Wayback:
         links = []
         for i in range(until,since, -1):
             self.open_url(year = i, web = web)
+            time.sleep(3)
+            urls = self.get_urls()
+            if urls:
+                links.extend(urls)
+            if i == 2014 and not links:
+                break
+        return links
+
+class Wayback:
+    '''
+    Scraps archived version from web.archive.org
+    Example of usage:
+    
+    scraper = Wayback()
+    scraper.open_chrome()
+    archived = []
+    for item in list_of_urls:
+        try:
+            archive = scraper.scan(item)
+            length = 0
+            if archive:
+                length = len(archive))
+                archived.append(archive)
+        except TimeoutException:
+            print('Error')
+    scraper.close_chrome()
+    '''
+    def __init__(self):
+        self.url = 'https://web.archive.org/web/{}*/{}'
+        
+    def open_chrome(self):
+        '''
+        opens chrome driver
+        '''
+        self.driver = webdriver.Chrome()
+        
+    def open_url(self,year = 2018, url = 'https://web.archive.org/web/{}*/{}'):
+        '''
+        opens website on web.archive.org
+        
+        args:
+            year: <int>: example 2018
+            url: <string>: 'https://www.oglaf.com/'
+        '''
+        self.driver.get(self.url.format(year, url))
+        
+    def get_text(self):
+        '''
+        returns website html content as string
+        '''
+        return self.driver.page_source
+    
+    def close_chrome(self):
+        '''
+        close chrome driver
+        '''
+        self.driver.close()
+        
+    def get_urls(self):
+        '''
+        returns found captures on webarchive
+        '''
+        captures = self.driver.find_elements_by_class_name('captures')
+        if captures:
+            return [item.find_element_by_tag_name('a').get_attribute('href') for item in captures]
+        else:
+            return []
+    
+    def scan(self, url = 'https://www.oglaf.com/', since = 2005, until = 2018):
+        '''
+        returns archive webs for url from since to until, it goes from until to since, if there are no points to 2014, it stops
+        
+        args:
+            url: <string>, example 'https://www.oglaf.com/'
+            since: <int>, example 2005, it needs to be lower than since
+            until: <int>, example 2018, it needs to be higher than until
+        '''
+        links = []
+        for i in range(until,since, -1):
+            self.open_url(year = i, url = url)
             time.sleep(3)
             urls = self.get_urls()
             if urls:
