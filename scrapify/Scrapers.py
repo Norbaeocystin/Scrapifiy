34f9bd38f2c2e389121ba6fc524ebc2f2250637c
Norbaeocystin <baranrastislav@gmail.com>
2019-07-11 21:33:52 +0200

    Update Scrapers.py

--- scrapify/Scrapers.py
+++ scrapify/Scrapers.py
@@ -30,11 +30,11 @@ class Scraper:
         self.driver = False
         if driver:
             self.driver = driver
-    
+
     def __get(self, url, proxies = {}):
         '''
         return get request
-        
+
         args:
             url: <string>, example: 'https://github.com/Norbaeocystin'
             proxies: <dict>, example: proxies: {'http': 'http://10.10.1.10:3128','https': 'http://10.10.1.10:1080'}
@@ -49,33 +49,33 @@ class Scraper:
                 return r
         else:
             return driver.get(url).page_source
-                       
+
     def __soupify(self, text):
         '''
         return BeautifulSoup object
-        
+
         args:
             text: <string>, html_content
         '''
         return BeautifulSoup(text, 'lxml')
-    
+
     def get_text(self, url, proxies = {}):
         '''
         return text content of GET request as string ( html_content)
-        
+
         args:
             url: <string>, example: 'https://github.com/Norbaeocystin'
             proxies: <dict>, example: proxies: {'http': 'http://10.10.1.10:3128','https': 'http://10.10.1.10:1080'}
-        ''' 
+        '''
         if self.driver:
             return self.driver.page_source
         else:
             return self.__get(url, proxies = proxies).text
-                       
+
     def get_soup(self, url, proxies = {}):
         '''
         return BeautifulSoup object from html content of GET request
-        
+
         args:
             url: <string>, example: 'https://github.com/Norbaeocystin'
             proxies: <dict>, example: proxies: {'http': 'http://10.10.1.10:3128','https': 'http://10.10.1.10:1080'}
@@ -91,22 +91,22 @@ class Scraper:
     def get_clean_text(self, url, proxies = {}):
         '''
         return text from html content of GET request
-        
+
         args:
             url: <string>, example: 'https://github.com/Norbaeocystin'
             proxies: <dict>, example: proxies: {'http': 'http://10.10.1.10:3128','https': 'http://10.10.1.10:1080'}
         '''
         soup = self.get_soup(url, proxies = {})
-	for script in soup.findAll('script')
+	    for script in soup.findAll('script')
             script.decompose()
-	for style in soup.findAll('style')
+	    for style in soup.findAll('style')
             style.decompose()
         return soup.text
-    
+
     def get_from_tag(self, tag_soup_object, dict_identificators):
         '''
         Example of usage
-        tags = {'Name':['h2',{'itemprop':'name'}], 'Street':['span',{'itemprop':'streetAddress'}], 
+        tags = {'Name':['h2',{'itemprop':'name'}], 'Street':['span',{'itemprop':'streetAddress'}],
                 'City':['span',{'itemprop':'addressLocality'}],
                 'Country':['span',{'itemprop':'addressCountry'}]}
         self.get_from_tag(soup, tags)
@@ -124,7 +124,7 @@ class Scraper:
             except AttributeError:
                 pass
         return data
-    
+
     def get_data(self, soup_object):
         '''
         returns dict of values from BeautifulSoup object
@@ -132,12 +132,12 @@ class Scraper:
         args:
             soup_object: <bs4.BeautifulSoup>
         '''
-        return {}      
+        return {}
 
     def get_data_for_urls(self, urls, proxies):
         '''
         returns list of dictionaries of scraped values
-        
+
         args:
             urls: <list>, example: ['https://github.com/Norbaeocystin']
             proxies: <dict>, example: proxies: {'http': 'http://10.10.1.10:3128','https': 'http://10.10.1.10:1080'}
@@ -145,12 +145,12 @@ class Scraper:
         self.urls = urls
         self.data =  [self.get_data(self.get_soup(item, proxies)) for item in self.urls]
         return self.data
-    
+
     def map_urls(self, urls, function):
         '''
 	also possibility to improve it to higher level with asyncio and aiohttp
         apply function over list of urls
-        
+
         args:
             urls: <list>, example: ['https://github.com/Norbaeocystin']
             function: <function>, example: self.get_data
@@ -187,30 +187,30 @@ class Scraper:
             executor.map(function, urls)
             #for item in executor.map(function, urls):
              #   data.update(item)
-    
+
     def __soupify(self, text):
         '''
         return BeautifulSoup object
-        
+
         args:
             text: <string>, html_content
         '''
         return BeautifulSoup(text, 'lxml')
-    
+
     def get_text(self, url, proxies = {}):
         '''
         return text content of GET request as string ( html_content)
-        
+
         args:
             url: <string>, example: 'https://github.com/Norbaeocystin'
             proxies: <dict>, example: proxies: {'http': 'http://10.10.1.10:3128','https': 'http://10.10.1.10:1080'}
-        '''         
+        '''
         return self.__get(url, proxies = proxies).text
-                       
+
     def get_soup(self, url, proxies = {}):
         '''
         return BeautifulSoup object from html content of GET request
-        
+
         args:
             url: <string>, example: 'https://github.com/Norbaeocystin'
             proxies: <dict>, example: proxies: {'http': 'http://10.10.1.10:3128','https': 'http://10.10.1.10:1080'}
@@ -219,11 +219,11 @@ class Scraper:
         #it creates attribute soup with BeautifulSoup object stored in the variable
         self.soup = self.__soupify(text)
         return self.soup
-    
+
     def get_from_tag(self, tag_soup_object, dict_identificators):
         '''
         Example of usage
-        tags = {'Name':['h2',{'itemprop':'name'}], 'Street':['span',{'itemprop':'streetAddress'}], 
+        tags = {'Name':['h2',{'itemprop':'name'}], 'Street':['span',{'itemprop':'streetAddress'}],
                 'City':['span',{'itemprop':'addressLocality'}],
                 'Country':['span',{'itemprop':'addressCountry'}]}
         self.get_from_tag(soup, tags)
@@ -241,7 +241,7 @@ class Scraper:
             except AttributeError:
                 pass
         return data
-    
+
     def get_data(self, soup_object):
         '''
         returns dict of values from BeautifulSoup object
@@ -249,13 +249,13 @@ class Scraper:
         args:
             soup_object: <bs4.BeautifulSoup>
         '''
-        return {}      
-                   
+        return {}
+
 class Wayback:
     '''
     Scraps archived version from web.archive.org
     Example of usage:
-    
+
     scraper = Wayback()
     scraper.open_chrome()
     archived = []
@@ -272,35 +272,35 @@ class Wayback:
     '''
     def __init__(self):
         self.url = 'https://web.archive.org/web/{}*/{}'
-        
+
     def open_chrome(self):
         '''
         opens chrome driver
         '''
         self.driver = webdriver.Chrome()
-        
+
     def open_url(self,year = 2018, url = 'https://web.archive.org/web/{}*/{}'):
         '''
         opens website on web.archive.org
-        
+
         args:
             year: <int>: example 2018
             url: <string>: 'https://www.oglaf.com/'
         '''
         self.driver.get(self.url.format(year, url))
-        
+
     def get_text(self):
         '''
         returns website html content as string
         '''
         return self.driver.page_source
-    
+
     def close_chrome(self):
         '''
         close chrome driver
         '''
         self.driver.close()
-        
+
     def get_urls(self, year, url):
         '''
         returns found captures on webarchive
@@ -312,11 +312,11 @@ class Wayback:
             return ['https://web.archive.org' + item['href'] for item in links]
         else:
             []
-    
+
     def scan(self, url = 'https://www.oglaf.com/', since = 2005, until = 2019):
         '''
         returns archive webs for url from since to until, it goes from until to since, if there are no points to 2014, it stops
-        
+
         args:
             url: <string>, example 'https://www.oglaf.com/'
             since: <int>, example 2005, it needs to be lower than since
@@ -335,7 +335,7 @@ class Wayback:
 PATTERN = r"\"?([-a-zA-Z0-9.`?{}]+@[-a-zA-Z0-9.`?{}]+[\.\w+]+)\"?"
 EMAILFINDER = re.compile(PATTERN)
 FILTER  = ['png', 'jpg', 'jpeg', '@gif', '@lg.x', '@md.x', '@sm.x', 'fontSize', '\d[.]\d', 'your@', 'mozilla',
-	   'javascript','\dpx', 'textAligncenter', 'marginTop', 'name@', 'wixpress.com','@yourname', 
+	   'javascript','\dpx', 'textAligncenter', 'marginTop', 'name@', 'wixpress.com','@yourname',
 	   'xs-only.x', 'com.avon.gi.rep.core.resman.vprov.ObjProvApplicationResource']
 #regex to find some crap in from abc@abc
 to_be_corrected =  '/@[A-Za-z]+$/'
@@ -368,17 +368,17 @@ class EmailScraper(Scraper):
         if emails:
             emails = [item for item in emails if '.' in item]
         if emails:
-            emails = [item for item in emails if not re.search('|'.join(FILTER), item, re.IGNORECASE)]   
+            emails = [item for item in emails if not re.search('|'.join(FILTER), item, re.IGNORECASE)]
         if emails:
-            emails = [item for item in emails if not '}' in item]  
+            emails = [item for item in emails if not '}' in item]
         if emails:
-            emails = [item for item in emails if not '{' in item]  
+            emails = [item for item in emails if not '{' in item]
         if emails:
             emails = [item for item in emails if not 'example' in item]
         if emails:
             emails = [item.replace('NOSPM','') for item in emails]
         return list(set(emails))
-    
+
     def find_contact_webpage(self, soup):
         '''
         in soup object try to find hyperlink for contact
